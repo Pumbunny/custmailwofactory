6eca2554fce0c4470adf576d9bcbe3800bcc1e0b
Pumbunny <50207097+Pumbunny@users.noreply.github.com>
2020-08-25 21:58:51 +0700

    Update CustomerMailApplication.java

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -16,42 +16,24 @@ public class CustomerMailApplication {
 
     /**
      * @param args the command line arguments
-     */
-    private Customer customer;
-    public void getCustomerTypeFromUser(String customerType) {
-        switch(customerType) {
-            case "Regular":
-                customer = new RegularCustomer();
-                break;
-            case "Mountain":
-                customer = new MountainCustomer();
-                break;
-            case "Delinquent":
-                customer = new DelinquentCustomer();
-                break;
-        }
-    }
-    public String generateMail() {
-        return customer.createMail();
-    }
-    
+     */   
     public static void main(String[] args) {
-        CustomerMailApplication app = new CustomerMailApplication();
+        //CustomerMailApplication app = new CustomerMailApplication();
+        CustomerFactory cusFac = new CustomerFactory();
         Scanner inp = new Scanner(System.in);
         System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
         int type = inp.nextInt();
         switch(type) {
             case 1:
-                app.getCustomerTypeFromUser("Regular");
+                System.out.println(cusFac.createCustomer("Regular").createMail());
                 break;
             case 2:
-                app.getCustomerTypeFromUser("Mountain");
+                System.out.println(cusFac.createCustomer("Mountain").createMail());
                 break;
             case 3:
-                app.getCustomerTypeFromUser("Delinquent");
+                System.out.println(cusFac.createCustomer("Delinquent").createMail());
                 break;
             
-        }
-        System.out.println(app.generateMail());        
+        }       
     }
 }
